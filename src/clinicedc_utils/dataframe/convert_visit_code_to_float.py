4181814d1eb6aa51d74e6f85a87412b6e44ca0a7
erikvw <ew2789@gmail.com>
2026-06-22 23:35:44 -0500

    convert_visit_code_to_float

--- src/clinicedc_utils/dataframe/convert_visit_code_to_float.py
+++ src/clinicedc_utils/dataframe/convert_visit_code_to_float.py
@@ -8,16 +8,21 @@ if TYPE_CHECKING:
 
 def convert_visit_code_to_float(df: pd.Dataframe) -> pd.Dataframe:
     """Convert visit_code to float using visit_code_sequence"""
-    if str(df["visit_code"].dtype) in ["object", "str"]:
-        df["visit_code_str"] = df["visit_code"]
-        df["visit_code_sequence_new"] = df["visit_code_sequence"]
-        df["visit_code_sequence_new"] = (
-            df["visit_code_sequence_new"]
-            .astype(float)
-            .apply(lambda x: x / 10.0 if x > 0.0 else 0.0)
-        )
-        df["visit_code"] = df["visit_code"].astype(float)
-        df["visit_code"] = df["visit_code"] + df["visit_code_sequence_new"]
-        df["visit_code"] = df["visit_code"].astype(float)
-        return df.drop(columns=["visit_code_sequence_new"])
-    return df
+    df = df.rename(columns={"visit_code": "visit_code_str"}).reset_index(drop=True)
+    df["visit_code"] = (
+        df["visit_code_str"].astype(str) + "." + df["visit_code_sequence"].astype(str)
+    ).astype("Float64")
+
+    # if str(df["visit_code"].dtype) in ["object", "str"]:
+    #     df["visit_code_str"] = df["visit_code"]
+    #     df["visit_code_sequence_new"] = df["visit_code_sequence"]
+    #     df["visit_code_sequence_new"] = (
+    #         df["visit_code_sequence_new"]
+    #         .astype(float)
+    #         .apply(lambda x: x / 10.0 if x > 0.0 else 0.0)
+    #     )
+    #     df["visit_code"] = df["visit_code"].astype(float)
+    #     df["visit_code"] = df["visit_code"] + df["visit_code_sequence_new"]
+    #     df["visit_code"] = df["visit_code"].astype(float)
+    #     return df.drop(columns=["visit_code_sequence_new"])
+    return df.reset_index(drop=True)
